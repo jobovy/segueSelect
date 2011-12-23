85791b0867b46ae2f3fadd9d2ae7862ee2249f81
Jo Bovy <bovy@ias.edu>
2011-12-23 12:05:33 -0500

    make default logg cut more conservative

--- segueSelect/segueSelect.py
+++ segueSelect/segueSelect.py
@@ -1240,7 +1240,7 @@ def read_gdwarfs(file=_GDWARFALLFILE,logg=False,ug=False,ri=False,sn=True,
     PURPOSE:
        read the spectroscopic G dwarf sample
     INPUT:
-       logg= if True, cut on logg, if number, cut on logg > the number (3.75)
+       logg= if True, cut on logg, if number, cut on logg > the number (>4.2)
        ug= if True, cut on u-g, if list/array cut to ug[0] < u-g< ug[1]
        ri= if True, cut on r-i, if list/array cut to ri[0] < r-i< ri[1]
        sn= if False, don't cut on SN, if number cut on SN > the number (15)
@@ -1265,7 +1265,7 @@ def read_gdwarfs(file=_GDWARFALLFILE,logg=False,ug=False,ri=False,sn=True,
     raw= raw[indx]
     #Cut on logg?
     if (isinstance(logg,bool) and logg):
-        indx= (raw.field('logga') > 3.75)
+        indx= (raw.field('logga') > 4.2)
         raw= raw[indx]
     elif not isinstance(logg,bool):
         indx= (raw.field('logga') > logg)
@@ -1311,7 +1311,7 @@ def read_kdwarfs(file=_KDWARFALLFILE,logg=False,ug=False,ri=False,sn=True,
     PURPOSE:
        read the spectroscopic K dwarf sample
     INPUT:
-       logg= if True, cut on logg
+       logg= if True, cut on logg (default: >4.2)
        ug= if True, cut on u-g
        ri= if True, cut on r-i
        sn= if False, don't cut on SN
@@ -1336,7 +1336,7 @@ def read_kdwarfs(file=_KDWARFALLFILE,logg=False,ug=False,ri=False,sn=True,
     raw= raw[indx]
     #Cut on logg?
     if isinstance(logg,bool) and logg:
-        indx= (raw.field('logga') > 3.75)
+        indx= (raw.field('logga') > 4.2)
         raw= raw[indx]
     elif not isinstance(logg,bool):
         indx= (raw.field('logga') > logg)
